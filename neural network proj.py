de1bcfd68fdd13c3970555d0bd08bc8e07fa3cde
Avniieee <avninehru988@gmail.com>
2024-07-16 22:35:58 +0530

    Update neural network proj.py

--- neural network proj.py
+++ neural network proj.py
@@ -17,8 +17,7 @@ df['Purchase'] = labels
 from sklearn.model_selection import train_test_split
 
 # Split the data
-X_train, X_test, y_train, y_test = train_test_split
-(df[['VisitDuration','PagesVisited']], df['Purchase'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df[['VisitDuration','PagesVisited']], df['Purchase'], test_size=0.2, random_state=42)
 
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
